43c9529612c735f7790f82b3fd2d0991d745f0de
Haseebismail703 <haseebismail703@gmail.com>
2023-10-26 21:52:30 +0500

    5

---
+++ chap 41t45/app.js
@@ -0,0 +1,67 @@
+
+
+     // WHILE LOOP
+
+   
+//     var a = 1
+//      while(a <=10) {
+//     document.write(a,'<br>')
+//     a++
+//    }                        
+
+
+// do...while loops
+
+
+// var a = 1
+
+// do{
+//     document.write(a,'<br>')
+//     a++
+// }
+// while(a<=10)
+
+// condition false milagi lkn phir bhi ak dafa chala ga
+
+// var a = 10
+
+// do{
+//     document.write(a,'<br>')
+//     --a
+// }
+// while(a<=1)
+
+
+
+
+
+    // comminting
+
+
+/*
+    function fun(){
+        alert('hi')
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
