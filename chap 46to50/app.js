3dd96ca64c771c3d99249a1ee4fc7f0841f99d36
Haseebismail703 <haseebismail703@gmail.com>
2023-10-26 22:24:12 +0500

    2

---
+++ chap 46to50/app.js
@@ -0,0 +1,7 @@
+// onclick
+// dobleclick
+// onMouseover
+// onFocus  form or input pai work karta input ka bacground colour change karta hai 
+// onBlur       ===                                     ===
+// oncontexmenu    mouse ka right click karna per function call hoga 
+
