9c003e468239ac127248f3caa44db5d6dfbde3f1
Haseebismail703 <haseebismail703@gmail.com>
2023-10-26 22:50:09 +0500

    3

--- chap 46to50/app.js
+++ chap 46to50/app.js
@@ -5,3 +5,34 @@
 // onBlur       ===                                     ===
 // oncontexmenu    mouse ka right click karna per function call hoga 
 
+
+
+// Reading field values  49
+
+
+// function check(){
+//     if(document.getElementById('email').value === ''){
+//         alert('enter email')
+//     }
+// }
+
+
+// Setting field values 50 
+
+
+//  function fillCity() {
+//      var cityName;
+//      var zipEntered = document.getElementById("zip").value;
+//      switch (zipEntered) {
+//      case "60608" :
+//      cityName = "Chicago";
+//      break;
+//      case "68114" :
+//      cityName = "Omaha";
+//      break;
+//      case "53212" :
+//      cityName = "Milwaukee";
+//      }
+//     }
+
+
