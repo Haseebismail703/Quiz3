7fa3a64c7093a0e644bc0ab521eb00c2997b5a0e
Haseebismail703 <haseebismail703@gmail.com>
2023-10-29 22:13:17 +0500

    5

---
+++ chap 51to55/app.js
@@ -0,0 +1,32 @@
+// Reading and setting paragraph text  51
+// see more 
+
+// function abc(){
+//     document.getElementById('ttt').innerHTML='Haseeb ur rehman'
+//     document.getElementById('anc').style.display='none'
+// }
+
+   
+
+// Manipulating images and text 52 
+
+
+// src 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
