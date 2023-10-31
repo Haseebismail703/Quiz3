7e576e516c6dd3d17dbd521a3406333f2933d1b5
Haseebismail703 <haseebismail703@gmail.com>
2023-10-31 22:59:02 +0500

    hi

---
+++ The dom/app.js
@@ -0,0 +1,21 @@
+// var arr = ['12','123','1231','123','456']
+
+// var filter = arr.filter(function(value){
+//     return value < 150
+// })
+// document.write(filter)
+
+// forEach
+// 
+// var arr = ['12','123','1231','123','456']
+
+// arr.forEach(function(value){
+//     document.write(value)
+// })
+
+// map new array return karaga 
+// var arr = ['12','123','1231','123','456']
+
+// arr.forEach(function(value){
+//     document.write(value)
+// })
